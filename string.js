54fb0cd040247a92df3c56aaf710b74b42d6f4f7
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2026-02-10 12:37:18 +0530

    Tester2

--- string.js
+++ string.js
@@ -2,3 +2,17 @@ let str = "hello";
 let reversed = str.split("").reverse().join("");
 
 console.log(reversed);
+
+let number = 7;
+let isPrime = true;
+
+if (number <= 1) isPrime = false;
+
+for (let i = 2; i < number; i++) {
+  if (number % i === 0) {
+    isPrime = false;
+    break;
+  }
+}
+
+console.log(isPrime ? "Prime number" : "Not a prime number");
